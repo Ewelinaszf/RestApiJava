b99eea744b8b1ed925cc4237894d521cafa2d8d5
eszymaniak <ewelina.szymaniak@ext.soprasteria.com>
2023-08-23 13:41:42 +0200

    First commit

--- src/main/java/Exercises/Exercise1/Calculator.java
+++ src/main/java/Exercises/Exercise1/Calculator.java
@@ -1,18 +1,26 @@
 package Exercises.Exercise1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
 
     public static void main(String[] args) {
-
+        int a = takeNumber();
+        int b = takeNumber();
+        System.out.println("Suma liczb to "+(a+b));
     }
 
 
-    public int takeNumber() {
+    public static int takeNumber() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj liczbę: ");
-        int number = scanner.nextInt();
+        int number = 0;
+        try {
+            number = scanner.nextInt();
+        }catch (InputMismatchException exception){
+            System.out.println("Niepoprawny format");
+        }
         return number;
     }
 }
